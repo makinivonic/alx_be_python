320a894798e91ac83c33f69b10f915408116e7b5
makinivonic <makinivonic@gmail.com>
2025-10-06 16:23:39 +0300

    user input calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,4 @@
+age = int(input ("How old are you?"))
+diffrence = 2050 - 2023
+future_age = age + diffrence
+print (f"In 2050, you will be {future_age} years old.")
--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,3 +1,12 @@
 hours = 2
 seconds = hours * 3600
 print (f"{hours} hour(s) is {seconds} seconds")
+
+# def convert(hours):
+#     seconds = hours * 3600
+#     print(f"{hours} hour(s) is {seconds} seconds")
+
+# convert(1)
+# convert(2)
+# convert(3)
+# convert(4)
