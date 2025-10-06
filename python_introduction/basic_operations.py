e30edb8fbd504df73906abc9e793691ba9468236
makinivonic <makinivonic@gmail.com>
2025-10-06 15:11:28 +0300

    exercise 1

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+# sum_of_numbers = number2 + number1
+# difference_of_numbers = number1 - number2
+# product_of_numbers = number2 * number1
+
+print(f"Addition of {number1} and {number2} is {number1 + number2}")
+print(f"Subtraction of {number2} from {number1} is {number1 - number2}" )
+print(f"Product of {number2} and {number1} is {number1 * number2}")
+
