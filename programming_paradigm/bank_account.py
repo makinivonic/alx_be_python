bda5878777182e1fa25fbcba3701dda3d2f3018e
makinivonic <makinivonic@gmail.com>
2025-10-24 16:12:52 +0300

    OOP: bank account initialize bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -23,6 +23,6 @@ class BankAccount:
             raise Exception(f"An Error has occurred. {e}")
     
     def display_balance(self):
-        print("Your balance is: ", self.account_balance)
+        print("Current Balance: ", self.account_balance)
 
       
