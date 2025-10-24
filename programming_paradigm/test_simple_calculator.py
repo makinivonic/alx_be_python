7ea4dc74b67458bc99c87b124c32e681f5a24c0b
makinivonic <makinivonic@gmail.com>
2025-10-24 17:29:26 +0300

    Test: seperate concerns to individual test function

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -3,14 +3,22 @@ from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
     """Test the perfomance of simple calculator class."""
+    def setUp(self):
+        self.calc = SimpleCalculator()
 
-    def test_calculator(self):
-        calc = SimpleCalculator()
-        self.assertEqual(calc.add(1, 5), 6)
-        self.assertEqual(calc.add(10, 10),20)
-        self.assertEqual(calc.subtract(10, 5), 5)
-        self.assertEqual(calc.divide(10, 5), 2)
-        self.assertIsNone(calc.divide(10, 0))
-        self.assertEqual(calc.multiply(10, 5), 50)
+    def test_addition(self):
+         self.assertEqual(self.calc.add(1, 5), 6)
+         self.assertEqual(self.calc.add(10, 10),20)
+    
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+    
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10, 5), 2)
+        self.assertIsNone(self.calc.divide(10, 0))
+        
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(10, 5), 50)
 
+        
     
