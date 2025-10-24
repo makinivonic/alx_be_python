673c04e9b0d1eff6edf0ba6ef63b73fadb2dd4b1
makinivonic <makinivonic@gmail.com>
2025-10-24 16:09:31 +0300

    OOP: bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,28 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+    
+    def deposit(self, amount):
+        try:
+            self.account_balance += amount
+        except TypeError:
+            raise TypeError("The amount must be a number.")
+        except Exception as e:
+            raise Exception(f"An Error has occurred. {e}")
+    
+    def withdraw(self, amount):
+        amount = float(amount)
+        try:
+            if amount > self.account_balance:
+                return False
+            self.account_balance -= amount
+            return True
+        except TypeError:
+            raise TypeError("The amount must be a number.")
+        except Exception as e:
+            raise Exception(f"An Error has occurred. {e}")
+    
+    def display_balance(self):
+        print("Your balance is: ", self.account_balance)
+
+      
